26f000052ac7189a2bd2ad72be800a7f065f5198
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-17 15:45:42 -0400

    Add project files.

---
+++ MovieRatings/Models/Business.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using MovieRatingsDB;
+
+namespace MovieRatings.Models
+{
+    public class Business
+    {
+        public static IEnumerable<Rating> GetRatings()
+        {
+            return DB.GetRatings();
+        }
+
+        public static Rating GetRating(int id)
+        {
+            return DB.GetRating(id);
+        }
+    }
+}
